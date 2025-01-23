4a49153c274572a1dee24285ea5a24668a809382
Nicole Jocson <nicpjocson@gmail.com>
2025-01-24 01:50:50 +0800

    Refactor

--- angle_estimator.py
+++ angle_estimator.py
@@ -21,80 +21,12 @@ angle_counts = {
 }
 
 # while webcam is open
-while cap.isOpened():
-    # read image from webcam
-    success, image = cap.read()
-
-    # flip image horizontally for a later selfie-view display (i.e. not mirrored/flipped)
-    # convert color space from BGR to RGB
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-    # for improving performance
-    # makes it so program can only read from the image
-    image.flags.writeable = False
-
-    # get result (returns normalized values)
-    results = face_mesh.process(image)
-
-    # for improving performance
-    # can write on image (i.e. display text, etc)
-    image.flags.writeable = True
-
-    # image height, width, and number of channels
-    # for scaling values with image dimensions
-    img_h, img_w, img_c = image.shape
-    face_3d = []
-    face_2d = []
-
-    # if there are detections
-    if results.multi_face_landmarks:
-        # run thru all landmarks detected in the image
-        for face_landmarks in results.multi_face_landmarks:
-            for idx, lm in enumerate(face_landmarks.landmark):
-                # indexes for e.g. nose, ears, mouth, eyes
-                # TODO: can use more points
-                if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
-                    # coordinates of landmark (normalized values)
-                    # scale with height and width of image (i.e. convert back to image space)
-                    x, y = int(lm.x * img_w), int(lm.y * img_h)
-
-                    # get 2D coordinates
-                    face_2d.append([x, y])
-                    # get 3D coordinates
-                    face_3d.append([x, y, lm.z])
-
-            # convert 2D coordinates to numpy array
-            face_2d = np.array(face_2d, dtype=np.float64)
-            # covert 3D coordinates to numpy array
-            face_3d = np.array(face_3d, dtype=np.float64)
-
-            # set up camera matrix
-            focal_length = 1 * img_w
-
-            # intrinsic parameters of camera
-            cam_matrix = np.array([[focal_length, 0, img_h / 2], 
-                                   [0, focal_length, img_w / 2], 
-                                   [0, 0, 1]])
 for root, _, files in os.walk(directory):
     for filename in files:
         if filename.endswith(".mp4"):
             print(filename)
             cap = cv2.VideoCapture(os.path.join(directory, filename))
             
-            # distortion parameters
-            dist_matrix = np.zeros((4, 1), dtype=np.float64)
-
-            # solve PnP
-            # rot_vec = how much the points are rotated in the image
-            # trans_vec = how much the points are translated around
-            success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
-
-            # get rotational matrix
-            rmat, jac = cv2.Rodrigues(rot_vec)
-
-            # get angles for all axes (normalized)
-            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-
             # get the y rotation degree
             x = angles[0] * 360
             y = angles[1] * 360
@@ -113,6 +45,52 @@ for root, _, files in os.walk(directory):
                 text = "Looking Up"
             else:
                 text = "Forward"
+            # FIX: NOT ENTERING THIS LOOP
+            while cap.isOpened():
+                success, image = cap.read()
+                if not success:
+                    break
+                else:
+
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+                results = face_mesh.process(image)
+                image.flags.writeable = True
+
+                # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                img_h, img_w, img_c = image.shape
+                face_3d = []
+                face_2d = []
+
+                if results.multi_face_landmarks:
+                    print("test")
+                    for face_landmarks in results.multi_face_landmarks:
+                        for idx, lm in enumerate(face_landmarks.landmark):
+                            # TODO: can use more points
+                            if idx in [33, 263, 1, 61, 291, 199]:
+                                # if idx == 1:
+                                #     nose_2d = (lm.x * img_w, lm.y * img_h)
+                                #     nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+
+                                x, y = int(lm.x * img_w), int(lm.y * img_h)
+                                face_2d.append([x, y])
+                                face_3d.append([x, y, lm.z])
+
+                        face_2d = np.array(face_2d, dtype=np.float64)
+                        face_3d = np.array(face_3d, dtype=np.float64)
+
+                        focal_length = 1 * img_w
+                        cam_matrix = np.array([[focal_length, 0, img_h / 2], 
+                                            [0, focal_length, img_w / 2], 
+                                            [0, 0, 1]])
+                    
+                        dist_matrix = np.zeros((4, 1), dtype=np.float64)
+
+                        success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+                        rmat, jac = cv2.Rodrigues(rot_vec)
+                        angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
 
                         # display nose direction
                         # nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
