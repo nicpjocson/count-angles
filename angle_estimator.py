6e622d978e39373edee63b2c227f9ff622377708
Nicole Jocson <nicpjocson@gmail.com>
2025-01-28 07:20:17 +0800

    Add calibrated webcam

--- angle_estimator.py
+++ angle_estimator.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time
+# import pickle
 
 mp_face_mesh = mp.solutions.face_mesh # open face mesh detector
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -9,6 +10,17 @@ face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_con
 mp_drawing = mp.solutions.drawing_utils # for displaying whole face mesh
 # drawing specifications
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+# # load camera matrix and distortion coefficients
+# with open("cameraMatrix.pkl", "rb") as f:
+#     cam_matrix = pickle.load(f)
+
+# with open("dist.pkl", "rb") as f:
+#     dist_matrix = pickle.load(f)
+
+# # test
+# print("Camera Matrix:\n", cam_matrix)
+# print("Distortion Coefficients:\n", dist_matrix)
 
 # open webcam
 cap = cv2.VideoCapture(0)
