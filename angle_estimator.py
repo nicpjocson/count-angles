8b593a3a7707ba4147d060d1af10c314fad27bbc
Nicole Jocson <nicpjocson@gmail.com>
2025-02-10 10:49:37 +0800

    Update angle_estimator.py

--- angle_estimator.py
+++ angle_estimator.py
@@ -6,8 +6,8 @@ import shutil
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+# mp_drawing = mp.solutions.drawing_utils
+# drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 '''
 Classify angle based on y-axis rotation.
